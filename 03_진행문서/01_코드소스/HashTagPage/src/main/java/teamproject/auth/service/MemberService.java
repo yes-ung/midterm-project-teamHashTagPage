faccada7e96b57bf45187076f10f8d320215e62a
yes-ung <yete0401@naver.com>
2025-08-11 10:19:18 +0900

    Delete 03_진행문서 directory

--- 03_진행문서/01_코드소스/HashTagPage/src/main/java/teamproject/auth/service/MemberService.java
+++
@@ -1,6 +0,0 @@
-package teamproject.auth.service;
-
-public interface MemberService {
-	MemberVO authenticate(MemberVO loginVO) throws Exception; // 로그인
-	void register(MemberVO memberVO) throws Exception;         // 회원가입
-}
